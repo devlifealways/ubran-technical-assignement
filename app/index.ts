a4791f393aa181857ee53922cf8b3680e89eab06
Hamza Rouineb <hamza@rouineb.com>
2022-07-25 07:40:35 +0200

    refactor: make it possible to override server port
    based on environment variables we could change app behavior
    at bootstrap phase (using configmap for instance)

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,10 @@
 import * as express from 'express';
 import 'express-async-errors';
 
+const SERVER_PORT = process.env.PORT || 3000;
+const environment = process.env.NODE_ENV || "development";
+const isDev = environment === "development";
+
 const app = express();
 
 app.get('*', (req: express.Request, res: express.Response) => {
@@ -13,6 +17,6 @@ app.get('*', (req: express.Request, res: express.Response) => {
   res.status(200).send(response);
 });
 
-app.listen(3000, () => {
-  console.log('listening on 3000');
+app.listen(SERVER_PORT, () => {
+  console.log(`listening on ${SERVER_PORT} 🚀`);
 });
