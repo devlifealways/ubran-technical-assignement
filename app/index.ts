993911c3714df8d133b3800801ff1429f1f700c1
Hamza Rouineb <hamza@rouineb.com>
2022-07-25 07:45:35 +0200

    feat: add /metrics endpoint
    using prom-client we expose the number of metrics
    the the website has processed

--- app/index.ts
+++ app/index.ts
@@ -1,20 +1,33 @@
 import * as express from 'express';
 import 'express-async-errors';
+import * as client from 'prom-client';
 
 const SERVER_PORT = process.env.PORT || 3000;
 const environment = process.env.NODE_ENV || "development";
 const isDev = environment === "development";
 
 const app = express();
+const registry = new client.Registry();
+const counter = new client.Counter({
+  name: 'visitors',
+  help: 'Number of visitors',
+  registers: [registry],
+});
 
-app.get('*', (req: express.Request, res: express.Response) => {
+app.get('/', (req: express.Request, res: express.Response) => {
   const response = {
     hostname: req.hostname,
     uptime: process.uptime(),
     podname: process.env.HOSTNAME,
   };
 
+  counter.inc();
   res.status(200).send(response);
+});
+
+app.get("/metrics", async (req: express.Request, res : express.Response) => {
+  res.setHeader("Content-Type", client.register.contentType);
+  res.end(await registry.metrics());
 });
 
 app.listen(SERVER_PORT, () => {
